ba5df2e9ed2280837ad8aafada2450da62d47a69
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-07-22 18:06:31 +0800

    Update buy.js

--- buy.js
+++ buy.js
@@ -1,38 +1,29 @@
+// 确保 pattern 匹配目标 URL
+buy = type=http-request,pattern=^https://cart.production.store-web.dynamics.com/v1.0/Cart/RequestParentalApproval\?appId=BuyNow,requires-body=1,max-size=0,binary-body-mode=0,timeout=90,script-path=https://raw.githubusercontent.com/iidamie/paga/main/xbox/buy.js,script-update-interval=0
+
+// buy.js 脚本内容
 let storedCartId = $persistentStore.read("cartId");
 
-// 根据是否成功读取购物车 ID 发送不同的通知
 if (storedCartId) {
-    $notification.post("Cart ID 提取成功", `Cart ID: ${storedCartId}`, "");
-    console.log(`读取到的 Cart ID: ${storedCartId}`);
-} else {
-    $notification.post("Cart ID 提取失败", "未能读取到 Cart ID", "");
-    console.log("未能读取到 Cart ID");
-}
+    let headers = $request.headers;
+    let body = JSON.parse($request.body);
+    
+    // 检查是否存在 cartId 字段并替换
+    if (body.cartId) {
+        console.log(`原始 Cart ID: ${body.cartId}`);
+        body.cartId = storedCartId;
+        console.log(`修改后的 Cart ID: ${body.cartId}`);
+    }
 
-let headers = $request.headers;
-let body = JSON.parse($request.body);
-
-// 修改请求体中的 cartId
-if (body.cartId) {
-    console.log(`原始 Cart ID: ${body.cartId}`);
-    body.cartId = storedCartId;
-    console.log(`修改后的 Cart ID: ${body.cartId}`);
-}
-
-// 修改请求头中的市场代码
-if (headers['X-MS-Market']) {
-    console.log(`原始 X-MS-Market: ${headers['X-MS-Market']}`);
+    // 可以根据需要修改请求头
     headers['X-MS-Market'] = 'NG';
-    console.log(`修改后的 X-MS-Market: ${headers['X-MS-Market']}`);
-}
-if (headers['x-ms-market']) {
-    console.log(`原始 x-ms-market: ${headers['x-ms-market']}`);
-    headers['x-ms-market'] = 'NG';
-    console.log(`修改后的 x-ms-market: ${headers['x-ms-market']}`);
-}
 
-// 发送修改后的请求
-$done({
-    headers: headers,
-    body: JSON.stringify(body)
-});
+    // 使用修改后的数据完成请求
+    $done({
+        headers: headers,
+        body: JSON.stringify(body)
+    });
+} else {
+    $notification.post("购物车 ID 未存储", "无法修改请求体因为没有存储的 Cart ID", "");
+    $done();  // 结束处理，不修改请求
+}
