9325a329f6128d5fc06e9d6d52c43369621cd235
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-07-23 15:16:51 +0800

    Update getcartv2.js

--- getcartv2.js
+++ getcartv2.js
@@ -2,19 +2,19 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (obj.cart && obj.cart.id) {
+if (obj.cart && obj.cart.id && obj.cart.lineItems && obj.cart.lineItems.length > 0) {
     let cartId = obj.cart.id;
-    let lineItemId = obj.cart.lineItems[0].id; // 假设我们需要第一个 lineItem 的 ID
+    let lineItemId = obj.cart.lineItems[0].id;  // 假设我们总是需要第一个 lineItem 的 ID
 
-    // 存储 cart id 和 lineItem id
+    // 存储 cartId 和 lineItemId
     $persistentStore.write(cartId, "cartId");
     $persistentStore.write(lineItemId, "lineItemId");
 
     $notification.post("Cart ID 获取成功", `Cart ID: ${cartId}, LineItem ID: ${lineItemId}`, "");
     console.log(`Cart ID: ${cartId}, LineItem ID: ${lineItemId}`);
 } else {
-    $notification.post("Cart ID 获取失败", "响应体中未找到 cart id 或 lineItems", "");
-    console.log("响应体中未找到 cart id 或 lineItems");
+    $notification.post("Cart ID 获取失败", "响应体中未找到必需的数据", "");
+    console.log("响应体中未找到必需的数据");
 }
 
 $done({});
