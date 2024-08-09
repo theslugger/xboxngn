31f49f2c95f8384114d628642127578959901894
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-09 09:50:38 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,21 +1,20 @@
 /**
- * Surge脚本：拒绝含 "toutiao" 的请求
+ * Surge & Quantumult X脚本：拒绝含 "toutiao" 的请求
+ * 部署路径：https://raw.githubusercontent.com/theslugger/xboxngn/main/reject_toutiao.js
  */
 
 function rejectRequest(request) {
-    var url = request.url;
-    var pattern = /toutiao/;
-    if (pattern.test(url)) {
-        return {
-            response: {
-                status: 403,
-                headers: { "Content-Type": "text/plain" },
-                body: "This request has been blocked."
-            }
-        };
-    }
-    // 允许其他请求正常通过
-    return { response: request };
+    return {
+        response: {
+            status: 403,
+            headers: { "Content-Type": "text/plain" },
+            body: "Access to this resource has been denied."
+        }
+    };
 }
 
+// Surge
 $httpClient.post(rejectRequest);
+
+// Quantumult X
+$done(rejectRequest({}));
