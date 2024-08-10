e1e6053608c84ab753bd980c2b7ddc1212b11771
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-10 23:32:31 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,12 +1,14 @@
 /**
- * 此脚本用于通过字符串匹配删除响应体中的"legalNotices":["DlcRequiresGame"]
+ * 此脚本用于通过字符串匹配在响应体中替换productId和availabilityId的值
  */
 
 let body = $response.body;
 
-// 通过字符串替换来完全移除"legalNotices":["DlcRequiresGame"]
-const pattern = '"legalNotices":\\["DlcRequiresGame"\\],';
-body = body.replace(new RegExp(pattern, 'g'), '');
+// 替换productId的值
+body = body.replace('"productId":"9P8RZ65WCKW4"', '"productId":"9NZ4144B8SJP"');
+
+// 替换availabilityId的值
+body = body.replace('"availabilityId":"9QKDBKVM7BMF"', '"availabilityId":"9SG7TNMGV3C7"');
 
 // 保存修改后的响应体
 $done({ body });
