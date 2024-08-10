42f87360691578c09c13a5a4c327496051dbbafe
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-10 23:54:31 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,14 +1,14 @@
 /**
- * 此脚本用于全局替换响应体中的指定字符串
+ * 此脚本用于通过正则表达式全局替换请求体中的特定字符串
  */
 
-let body = $response.body;
+let body = $request.body;
 
-// 全局替换字符串 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
+// 使用正则表达式全局替换字符串 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
 body = body.replace(/9P8RZ65WCKW4/g, '9NZ4144B8SJP');
 
-// 全局替换字符串 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
+// 使用正则表达式全局替换字符串 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
 body = body.replace(/9QKDBKVM7BMF/g, '9SG7TNMGV3C7');
 
-// 保存修改后的响应体
+// 应用修改后的请求体
 $done({ body });
