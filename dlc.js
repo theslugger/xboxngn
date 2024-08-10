e9bda332f3613788fbea0e3451f18345c3cb099a
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-10 23:47:55 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,15 +1,14 @@
 /**
- * 此脚本用于通过正则表达式灵活匹配和替换响应体中的productId和availabilityId的值
+ * 此脚本用于全局替换响应体中的指定字符串
  */
 
 let body = $response.body;
 
-// 定义正则表达式，考虑到可能的空格和换行
-const productIdRegex = /("productId"\s*:\s*"9P8RZ65WCKW4")/;
-body = body.replace(productIdRegex, '"productId":"9NZ4144B8SJP"');
+// 全局替换字符串 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
+body = body.replace(/9P8RZ65WCKW4/g, '9NZ4144B8SJP');
 
-const availabilityIdRegex = /("availabilityId"\s*:\s*"9QKDBKVM7BMF")/;
-body = body.replace(availabilityIdRegex, '"availabilityId":"9SG7TNMGV3C7"');
+// 全局替换字符串 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
+body = body.replace(/9QKDBKVM7BMF/g, '9SG7TNMGV3C7');
 
 // 保存修改后的响应体
 $done({ body });
