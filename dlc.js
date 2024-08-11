2f127a490d3ac2c3c2a12d081e7a1a1a85fa66d2
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-11 19:37:33 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,10 +1,10 @@
 /**
- * 此脚本用于通过正则表达式全局替换请求体中的特定字符串
+ * 此脚本用于通过正则表达式全局替换响应体中的特定字符串
  */
 
-// 检查请求体是否存在
-if ($request.body) {
-    let body = $request.body;
+// 检查响应体是否存在
+if ($response.body) {
+    let body = $response.body;
 
     // 使用正则表达式全局替换字符串 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
     body = body.replace(/9P8RZ65WCKW4/g, '9NZ4144B8SJP');
@@ -12,9 +12,9 @@ if ($request.body) {
     // 使用正则表达式全局替换字符串 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
     body = body.replace(/9QKDBKVM7BMF/g, '9SG7TNMGV3C7');
 
-    // 应用修改后的请求体
+    // 应用修改后的响应体
     $done({ body });
 } else {
-    // 如果请求体不存在，直接返回原请求
+    // 如果响应体不存在，直接返回原响应
     $done({});
 }
