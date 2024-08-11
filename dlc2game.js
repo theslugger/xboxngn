24579dd9e15e55d98ed383338beecda02efcbfd7
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-11 15:08:18 +0800

    Update dlc2game.js

--- dlc2game.js
+++ dlc2game.js
@@ -1,31 +1,17 @@
 /**
- * 替换响应体中的指定字符串
- * @param {string} body - 原始响应体文本
- * @return {string} - 修改后的响应体文本
+ * Surge的响应体处理脚本
+ * 替换响应体中指定的字符串
+ * 
+ * @param {Object} $response - Surge提供的响应对象
+ * @returns {Object} - 修改后的响应对象
  */
-function replaceText(body) {
-    let modified = body.replace(/"type":"addOn","kind":"Consumable"/g, '"type":"games","kind":"Game"');
-    return modified;
-}
-
-/**
- * Surge的响应体处理函数
- * @param {Object} param - 包含响应体和其它信息的对象
- * @return {Object} - 返回修改后的响应对象
- */
-function httpResponseModify(param) {
-    if (param.response.body) {
-        let body = param.response.body;
-        let modifiedBody = replaceText(body);
-        let modifiedResponse = {
-            response: {
-                body: modifiedBody
-            }
-        };
-        return modifiedResponse;
-    } else {
-        return param;
+function replaceResponse($response) {
+    if ($response.body) {
+        let modifiedBody = $response.body.replace(/"type":"addOn","kind":"Consumable"/g, '"type":"games","kind":"Game"');
+        return { body: modifiedBody };
     }
+    return $response;
 }
 
-httpResponseModify($response);
+// 使用Surge提供的全局变量 $response 调用函数
+$done(replaceResponse($response));
