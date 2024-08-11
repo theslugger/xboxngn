6f377ec240dd7fbbe34a1ff9399b142848c7d973
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-11 19:49:43 +0800

    Update dlc1.js

--- dlc1.js
+++ dlc1.js
@@ -1,15 +1,18 @@
 /**
- * 此脚本用于通过正则表达式全局替换响应体中的特定字符串
+ * 此脚本用于通过字符匹配全局替换响应体中的特定字符串
  */
 
 // 获取响应体并直接进行修改
 let body = $response.body;
 
-// 使用正则表达式全局替换字符串 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
-body = body.replace(/9P8RZ65WCKW4/g, '9NZ4144B8SJP');
+// 字符串全局替换 "9P8RZ65WCKW4" 为 "9NZ4144B8SJP"
+while (body.includes("9P8RZ65WCKW4")) {
+    body = body.replace("9P8RZ65WCKW4", "9NZ4144B8SJP");
+}
 
-// 使用正则表达式全局替换字符串 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
-body = body.replace(/9QKDBKVM7BMF/g, '9SG7TNMGV3C7');
+// 字符串全局替换 "9QKDBKVM7BMF" 为 "9SG7TNMGV3C7"
+while (body.includes("9QKDBKVM7BMF")) {
+    body = body.replace("9QKDBKVM7BMF", "9SG7TNMGV3C7");
+}
 
-// 应用修改后的响应体
 $done({ body });
