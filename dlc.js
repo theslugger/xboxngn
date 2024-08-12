822da54ae8701a9d5e834183041be5eee0978861
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-12 21:02:08 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,11 +1,11 @@
 var url = $request.url;
 var body = $request.body;
 
-// 替换 URL 中的特定字符串
-url = url.replace(/9MW9469V91LM_0010/g, "BNBLPQM9MZ19_0X3J");
+// 使用边界检查来替换 URL 中的特定字符串
+url = url.replace(/\b9MW9469V91LM_0010\b/g, "BNBLPQM9MZ19_0X3J");
 
-// 替换请求体中的相同字符串
-body = body.replace(/9MW9469V91LM_0010/g, "BNBLPQM9MZ19_0X3J");
+// 使用边界检查来替换请求体中的相同字符串
+body = body.replace(/\b9MW9469V91LM_0010\b/g, "BNBLPQM9MZ19_0X3J");
 
 // 返回修改后的请求
 $done({url: url, body: body});
