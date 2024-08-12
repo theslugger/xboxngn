55a6afd6e18ca8f316c20b93e8000295922ff71f
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-12 17:04:07 +0800

    Update dlc1.js

--- dlc1.js
+++ dlc1.js
@@ -7,13 +7,15 @@ obj.pendingRequests.forEach(request => {
         request.extendedMessage.popupStoreUrl = `https://www.microsoft.com/store/buyboxlitev2?clientType=storewebsdk&market=AR&deviceFamily=mobile&locale=es-AR&noCanonical=true&layout=modal&currentTheme=light&brand=GamingApp&clientVersion=0.5.4&ask={"fsid":"${request.id}","cid":"${request.extendedMessage.cid || request.puid}","src":"site"}`;
     }
 
-    // 如果找到特定ID，更新消息和approvalUrl
-    if (request.id === "9NZ4144B8SJP_0010") {
-        var amount = request.extendedMessage.amount; // 获取金额，用于更新消息文本
+    // 动态构建message
+    var amount = request.extendedMessage.amount; // 获取金额，用于更新消息文本
+    if (amount) { // 确保amount存在
         request.message = `si 是否可以购买此 game (支付 <b>$ ${amount},00<\/b>)?`; // 动态更新消息文本
+    }
 
-        // 动态更新approvalUrl
-        var appID = request.id.slice(0, request.id.indexOf("_")); // 从ID获取appID部分
+    // 动态构建approvalUrl
+    var appID = request.id.slice(0, request.id.indexOf("_")); // 从ID获取appID部分
+    if (appID) { // 确保appID提取正确
         request.extendedMessage.approvalUrl = `https://www.microsoft.com/es-AR/store/apps/${appID}?ask={"fsid":"${request.id}","cid":"${request.extendedMessage.cid || request.puid}","src":"site"}&origin=familyapp`;
     }
 });
