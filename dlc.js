7e3ffffe5f8c2c92c4760c0c568d5916708d4fea
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-12 20:59:22 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,12 +1,11 @@
 var url = $request.url;
 var body = $request.body;
 
-// 正则表达式精确替换 URL 中的 fsid
-url = url.replace(/fsid=9MW9469V91LM_0010(?!\d)/g, "fsid=BNBLPQM9MZ19_0X3J");
+// 替换 URL 中的特定字符串
+url = url.replace(/9MW9469V91LM_0010/g, "BNBLPQM9MZ19_0X3J");
 
-// 修改请求体中的 ProductId 和 SkuId
-body = body.replace(/ProductId=9MW9469V91LM(?!\d)/g, "ProductId=BNBLPQM9MZ19");
-body = body.replace(/SkuId=0010(?!\d)/g, "SkuId=0X3J");
+// 替换请求体中的相同字符串
+body = body.replace(/9MW9469V91LM_0010/g, "BNBLPQM9MZ19_0X3J");
 
 // 返回修改后的请求
 $done({url: url, body: body});
