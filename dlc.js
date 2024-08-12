0a3d758f72838475148f2a4d7a15d03df95d5a98
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-12 20:04:55 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,23 +1,22 @@
 var url = $request.url;
-var body = $request.body;
 
-// 修改 URL 中的 fsid 参数
+// 替换 URL 中的 fsid 参数
 url = url.replace("9MW9469V91LM_0010", "BNBLPQM9MZ19_0X3J");
 
-// 检查是否需要修改查询字符串中的 JSON
-if (url.includes("buyboxlitev2") && body.includes("fsid\":\"9MW9469V91LM_0010")) {
-    // 假设查询字符串是以 JSON 格式发送的
-    var data = JSON.parse(decodeURIComponent(body));
-    if (data && data.fsid) {
-        data.fsid = data.fsid.replace("9MW9469V91LM_0010", "BNBLPQM9MZ19_0X3J");
-        // 重新编码为查询字符串
-        body = encodeURIComponent(JSON.stringify(data));
-    }
+// 解析 URL 查询字符串中的 `ask` 参数，并替换其中的 `fsid`
+var urlObj = new URL(url);
+var askParam = urlObj.searchParams.get("ask");
+if (askParam) {
+    var askData = JSON.parse(decodeURIComponent(askParam));
+    askData.fsid = askData.fsid.replace("9MW9469V91LM_0010", "BNBLPQM9MZ19_0X3J");
+    urlObj.searchParams.set("ask", encodeURIComponent(JSON.stringify(askData)));
 }
 
-// 输出修改后的请求
+// 重构 URL
+url = urlObj.toString();
+
+// 输出修改后的 URL 以供调试
 console.log("Modified URL: " + url);
-console.log("Modified Body: " + body);
 
 // 返回修改后的请求
-$done({url: url, body: body});
+$done({url: url});
