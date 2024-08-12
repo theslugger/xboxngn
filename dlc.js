f527bda3ce0f48fb0eb685c5442e6a2a94582d13
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-12 20:18:48 +0800

    Update dlc.js

--- dlc.js
+++ dlc.js
@@ -1,24 +1,30 @@
 var url = $request.url;
 var body = $request.body;
 
-// 使用正则表达式替换 URL 中的 fsid
-url = url.replace(/fsid=9MW9469V91LM_0010/g, "fsid=BNBLPQM9MZ19_0X3J");
+// 打印原始URL和请求体进行调试
+console.log("Original URL: " + url);
+console.log("Original Body: " + body);
 
-// 修改 URL query 中的 `ask` 参数
-var askMatches = url.match(/ask=([^&]+)/);
-if (askMatches && askMatches[1]) {
-    var decodedAsk = decodeURIComponent(askMatches[1]);
-    decodedAsk = decodedAsk.replace("9MW9469V91LM_0010", "BNBLPQM9MZ19_0X3J");
-    url = url.replace(askMatches[1], encodeURIComponent(decodedAsk));
+// 正则表达式替换 URL 中的 fsid
+url = url.replace(/fsid=9MW9469V91LM_0010(?!\d)/, "fsid=BNBLPQM9MZ19_0X3J");
+
+// 解析并替换URL查询字符串中的`ask`参数
+var askMatch = url.match(/ask=([^&]*)/);
+if (askMatch && askMatch[1]) {
+    let askDecoded = decodeURIComponent(askMatch[1]);
+    askDecoded = askDecoded.replace(/"fsid":"9MW9469V91LM_0010"(?!\d)/g, '"fsid":"BNBLPQM9MZ19_0X3J"');
+    url = url.replace(askMatch[1], encodeURIComponent(askDecoded));
 }
 
-// 修改请求体
+// 修改请求体中的ProductId和SkuId
 if (body) {
-    // 替换 ProductId
-    body = body.replace(/ProductId=9MW9469V91LM/g, "ProductId=BNBLPQM9MZ19");
-    // 替换 SkuId
-    body = body.replace(/SkuId=0010/g, "SkuId=0X3J");
+    body = body.replace(/ProductId=9MW9469V91LM(?!\d)/g, "ProductId=BNBLPQM9MZ19");
+    body = body.replace(/SkuId=0010(?!\d)/g, "SkuId=0X3J");
 }
+
+// 打印修改后的 URL 和 请求体供调试
+console.log("Modified URL: " + url);
+console.log("Modified Body: " + body);
 
 // 返回修改后的请求
 $done({url: url, body: body});
