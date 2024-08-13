4afa8149743511a10763c17c08918faea1d5432e
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-13 14:08:20 +0800

    Create dlc3.js

---
+++ dlc3.js
@@ -0,0 +1,3 @@
+var modifyBody = $request.body.replace(/"productId":"9MW9469V91LM","skuId":"0010","availabilityId":"B17K7P5V96W3"/, '"productId":"9NZ4144B8SJP","skuId":"0010","availabilityId":"9SG7TNMGV3C7"');
+
+$done({body: modifyBody});
