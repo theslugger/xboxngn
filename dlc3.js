cfcf7d1e26ab09ec20c609ae3f92c13211925107
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-13 15:09:22 +0800

    Update dlc3.js

--- dlc3.js
+++ dlc3.js
@@ -1,19 +1,19 @@
-// 定义原始和目标字符串
-const originalProductId = '"productId":"9MW9469V91LM"';
-const newProductId = '"productId":"9NZ4144B8SJP"';
-const originalAvailabilityIdd = '"availabilityIdd":"B17K7P5V96W3"';
-const newAvailabilityIdd = '"availabilityIdd":"9SG7TNMGV3C7"';
+// 检查是否存在请求体
+if ($request.body) {
+    // 定义原始和目标字符串
+    const originalProductId = '"productId":"9MW9469V91LM"';
+    const newProductId = '"productId":"9NZ4144B8SJP"';
+    const originalAvailabilityIdd = '"availabilityIdd":"B17K7P5V96W3"';
+    const newAvailabilityIdd = '"availabilityIdd":"9SG7TNMGV3C7"';
 
-// 检查请求体中是否包含需要替换的字符串
-if ($request.body.includes(originalProductId) && $request.body.includes(originalAvailabilityIdd)) {
     // 执行替换操作
     var modifyBody = $request.body
         .replace(originalProductId, newProductId)
         .replace(originalAvailabilityIdd, newAvailabilityIdd);
 
-    // 完成修改
+    // 完成修改并返回修改后的请求体
     $done({body: modifyBody});
 } else {
-    // 如果不包含目标字符串，则不做修改
+    // 如果请求体不存在，不做任何修改
     $done({});
 }
