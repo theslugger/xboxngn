2df29a0d7dac28f623969213e87bf965bd2cb219
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-13 15:36:42 +0800

    Update dlc3.js

--- dlc3.js
+++ dlc3.js
@@ -1,13 +1,28 @@
 // 检查是否存在请求体
 if ($request.body) {
-    // 使用正则表达式替换，考虑前后可能有空白字符、引号、冒号等符号
+    console.log("Original Body: " + $request.body); // 打印原始请求体
+
+    // 执行替换操作
     var modifiedBody = $request.body
-        .replace(/(["',:\s]){1}9MW9469V91LM(["',:\s]){1}/g, '$19NZ4144B8SJP$2')
-        .replace(/(["',:\s]){1}B17K7P5V96W3(["',:\s]){1}/g, '$19SG7TNMGV3C7$2');
+        .replace(/9MW9469V91LM/g, "9NZ4144B8SJP")
+        .replace(/B17K7P5V96W3/g, "9SG7TNMGV3C7");
+
+    console.log("Modified Body: " + modifiedBody); // 打印修改后的请求体
+
+    // 检查是否有变化
+    if (modifiedBody === $request.body) {
+        // 如果没有变化，发送一个通知
+        $notification.post('修改失败', '未替换任何内容', '');
+    } else {
+        // 如果有变化，发送一个成功的通知
+        $notification.post('修改成功', '内容已替换', '');
+    }
 
     // 完成修改并返回修改后的请求体
     $done({body: modifiedBody});
 } else {
-    // 如果请求体不存在，不做任何修改
+    // 如果请求体不存在，发送一个错误通知
+    console.log("No request body found.");
+    $notification.post('错误', '没有找到请求体', '');
     $done({});
 }
