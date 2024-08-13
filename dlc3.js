882e60ae846335d3e0670285f68b8c49421829e8
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-13 15:31:34 +0800

    Update dlc3.js

--- dlc3.js
+++ dlc3.js
@@ -1,9 +1,9 @@
 // 检查是否存在请求体
 if ($request.body) {
-    // 使用简单的字符串替换
+    // 使用正则表达式替换，考虑前后可能有空白字符、引号、冒号等符号
     var modifiedBody = $request.body
-        .replace(/9MW9469V91LM/g, "9NZ4144B8SJP")
-        .replace(/B17K7P5V96W3/g, "9SG7TNMGV3C7");
+        .replace(/(["',:\s]){1}9MW9469V91LM(["',:\s]){1}/g, '$19NZ4144B8SJP$2')
+        .replace(/(["',:\s]){1}B17K7P5V96W3(["',:\s]){1}/g, '$19SG7TNMGV3C7$2');
 
     // 完成修改并返回修改后的请求体
     $done({body: modifiedBody});
