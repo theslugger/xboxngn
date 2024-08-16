a5a5e6e4e13e2e86abf094528556e01d8e4c37a3
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-16 17:13:02 +0800

    Update img.js

--- img.js
+++ img.js
@@ -4,22 +4,27 @@ var body = $response.body;
 if (body) {
     var obj = JSON.parse(body);
 
-    // 检查 pendingRequests 数组中的每个元素
-    obj.pendingRequests.forEach(function(request) {
-        // 检查并修改 appName
-        if (request.appName && request.appName === "Forza Motorsport Premium Edition") {
-            request.appName = "Liga Overwatch™ - 100 Fichas de Liga";
-        }
+    // 定位两个特定的对象
+    var sourceItem = obj.pendingRequests.find(item => item.id === "BNBLPQM9MZ19_0X3J");
+    var targetItem = obj.pendingRequests.find(item => item.id === "9P8PGC771MLP_0010");
 
-        // 检查并修改 appIcon
-        if (request.appIcon && request.appIcon === "https://store-images.s-microsoft.com/image/apps.32557.14083713035901346.16619c9c-f1cb-4fd8-93b8-e5754d238bfe.f01e409f-c2ee-49ca-bfdb-ef770223751e?w=200&h=200&q=60") {
-            request.appIcon = "https://store-images.s-microsoft.com/image/apps.46981.67853844255942228.41453783-f2e6-44d2-9beb-e2557b254e8a.047c23cd-4c06-45f3-8da6-3e61e9f91104?w=200&h=200&q=60";
-        }
-    });
+    if (sourceItem && targetItem) {
+        // 替换 appName 和 appIcon
+        targetItem.appName = sourceItem.appName;
+        targetItem.appIcon = sourceItem.appIcon;
+
+        // 发送成功通知
+        $notification.post("替换成功", "已成功替换 appName 和 appIcon", "原名称: " + targetItem.appName + " 新名称: " + sourceItem.appName);
+    } else {
+        // 发送失败通知
+        $notification.post("替换失败", "未找到指定的对象", "请检查 ID 是否正确");
+    }
 
     // 重新封装修改后的响应内容并返回
     $done({body: JSON.stringify(obj)});
 } else {
     // 如果没有有效的响应体，直接返回未修改的响应
     $done({});
+    // 发送失败通知
+    $notification.post("操作失败", "响应体为空", "无数据可修改");
 }
